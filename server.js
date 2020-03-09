5f0de7f264ea3d5d9859ffec59bb0419ce859347
Arrodl <arrodl40@gmail.com>
2020-03-08 19:49:40 -0600

    fd

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ require('./app/routes/message.routes')(app);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}.`);
+// });
