5bb35ff6319fead5f788ceeb375492901964150e
Arrodl <arrodl40@gmail.com>
2020-03-08 19:14:54 -0600

    1337

--- server.js
+++ server.js
@@ -43,11 +43,9 @@ app.listen(PORT, () => {
 });
 
 const server = http.createServer(app);
-
 const io = socketIO(server);
-
 io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
 });
-server.listen(PORT);
+server.listen(1337);
