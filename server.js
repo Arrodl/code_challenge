8c35260a97c79e99e3022fb13a08e0182d7e0640
Arrodl <arrodl40@gmail.com>
2020-03-08 18:51:00 -0600

    dfs

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ var corsOptions = { origin: "http://localhost:3000" };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+});
 
 var connectedUsers = [];
 
