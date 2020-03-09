cdee6790476045266a6de6c521cd3cef159df7a6
Arrodl <arrodl40@gmail.com>
2020-03-08 19:36:36 -0600

    dfs

--- server.js
+++ server.js
@@ -36,7 +36,15 @@ app.listen(PORT, () => {
 });
 
 const server = http.createServer(app);
-const io = socketIO(server, { origins: '*:*' });
+const io = socketIO(server, { handlePreflightRequest: (req, res) => {
+    const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true
+    };
+    res.writeHead(200, headers);
+    res.end();
+} });
 io.origins('*:*')
 io.on('connection', (socket) => {
     console.log('Client connected');
